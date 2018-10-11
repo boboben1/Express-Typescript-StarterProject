37e2ffb8b7cba5eb9fe8f232b0db85c18c2f3517
boboben1 <boboben5@gmail.com>
2018-10-11 16:09:12 -0400

    Fixed babel. Changed some tsconfig settings. Fixed polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var merge = require('merge-stream');
 var plumber = require('gulp-plumber');
 
-gulp.task('default', function() {
-    var tsResult = tsProject.src()
+gulp.task('default', function () {
+	var tsResult = tsProject.src()
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
-        	.pipe(tsProject());
+		.pipe(tsProject());
 	var babelResult = tsResult.js
 		.pipe(plumber())
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
+		.pipe(babel({}))
 		.pipe(sourcemaps.write('./'));
 	return merge(tsResult.dts, babelResult)
-	.pipe(plumber())
-        .pipe(gulp.dest('./dist'));
+		.pipe(plumber())
+		.pipe(gulp.dest('./dist'));
 });
