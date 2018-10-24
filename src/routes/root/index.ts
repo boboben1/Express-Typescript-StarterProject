1e1f9fd5b682d16bec3c7dcbfbc7e4a941343105
boboben1 <boboben5@gmail.com>
2018-10-24 17:03:25 -0400

    Added 404

--- src/routes/root/index.ts
+++ src/routes/root/index.ts
@@ -13,4 +13,9 @@ login.use(router);
 router.use('/api', api);
 
 
+router.all('*', (req, res, next) => {
+    res.status(404).send('<h1>Error 404: Not found</h1>');
+});
+
+
 export {router as index};
